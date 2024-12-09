377617239ee7006c2931c1ebe0ca3eac409ce2a8
Cameron Anderson <canderson9756@gmail.com>
2024-12-09 11:53:08 +0000

    day 1 complete

--- day1/solution.py
+++ day1/solution.py
@@ -15,13 +15,22 @@ def data_loader_text(filename, delimiter='   '):
             list2.append(int(data_values[1]))
     return list1, list2
 
-def process_lists(list1, list2, diff_list=[]):
+def process_lists_part1(list1, list2, diff_list=[]):
     list1.sort()
     list2.sort()
     diff_list = [abs(i - j) for (i, j) in zip(list1, list2)]
     return sum(diff_list)
 
+def process_lists_part2(list1, list2):
+    similarity_score = 0
+    for value in list1:
+        similarity_score += list2.count(value) * value
+    return similarity_score
+
 if __name__=='__main__':
     list1, list2 = data_loader_text('input', '   ')
-    sum_value = process_lists(list1, list2)
-    print(sum_value)
+    sum_value = process_lists_part1(list1, list2)
+    similarity_score = process_lists_part2(list1, list2)
+
+    print("Part one (total distance): ", sum_value)
+    print("Part two (similarity score): ", similarity_score)
