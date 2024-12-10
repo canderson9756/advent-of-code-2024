4675c3178791d4ac344bb6ec33966025221ee4e1
Cameron Anderson <canderson9756@gmail.com>
2024-12-10 11:00:40 +0000

    Day 2 completed successfully

--- day2/solution.py
+++ day2/solution.py
@@ -4,7 +4,7 @@ def data_loader(filename):
         reports = [line.split(' ') for line in lines]
     return reports
 
-def process_report(report):
+def process_report_part1(report):
     diff = [int(j) - int(i) for i, j in zip(report, report[1:])]
     # Check conditions
     if (all(value > 0 for value in diff) or all(value < 0 for value in diff)):
@@ -13,12 +13,23 @@ def process_report(report):
                 return True
     return False
     
-def process_all_reports(reports):
-    processed_reports = [process_report(report) for report in reports]
+def process_report_part2(report):
+    for i in range(len(report)):
+        list1 = report[:i]
+        list2 = report[i+1:]
+        task_removed_list = list1 + list2
+        processed_report = process_report_part1(task_removed_list)
+        if processed_report:
+            return True
+
+def process_all_reports(reports, part='1'):
+    processed_reports = [process_report_part1(report) for report in reports] if part == '1' else [process_report_part2(report) for report in reports]
     return processed_reports.count(True)
 
 
 if __name__ == '__main__':
     reports = data_loader('input')
     safe_reports = process_all_reports(reports)
-    print('Number of safe reports meeting criteria: ', safe_reports)
+    print('Number of safe reports meeting criteria (part 1): ', safe_reports)
+    safe_reports2 = process_all_reports(reports=reports, part='2')
+    print('Number of safe reports meeting criteria (part 2): ', safe_reports2)
